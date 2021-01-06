13fcd48c990fa9eece3fa867910641273fac38ba
creattech <crystalsmcneil@gmail.com>
2021-01-05 20:58:05 -0600

    adds landing page and privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing_page'
+  get 'static_pages/privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

72748b21ec372c9b6ee360d25b45a6c82674ee5b
creattech <crystalsmcneil@gmail.com>
2021-01-07 19:16:45 -0600

    adds routes to landing page and privacy policy page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#landing_page'
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 end

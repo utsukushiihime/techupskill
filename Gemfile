232091dfa41a6431e1c55f47f26e0d635df5c869
creattech <crystalsmcneil@gmail.com>
2021-01-10 00:25:25 -0600

    adds friendly_id

--- Gemfile
+++ Gemfile
@@ -34,3 +34,4 @@ end
 
 gem 'simple_form'
 gem 'devise'
+gem 'friendly_id', '~> 5.4.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_10_042834) do
+ActiveRecord::Schema.define(version: 2021_01_10_062257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2021_01_10_042834) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 

c0eacadd97e4c3a61a19ae4d6c64717bffc924f4
creattech <crystalsmcneil@gmail.com>
2021-01-14 23:24:39 -0600

    updates webpack

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -1,1 +1,12 @@
 // config/webpack/custom.js
+module.exports = {
+    resolve: {
+        alias: {
+            jquery: 'jquery/src/jquery',
+            vue: 'vue/dist/vue.js',
+            React: 'react',
+            ReactDOM: 'react-dom',
+            vue_resource: 'vue-resource/dist/vue-resource'
+        }
+    }
+}
